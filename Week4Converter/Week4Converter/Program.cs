eaecb00f62f1b601bba73794afeeae4c97480d8e
Szymon J <szymon.j@hotmail.com>
2019-02-11 10:21:35 +0000

    Created converter project

---
+++ Week4Converter/Week4Converter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Converter
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //Console.WriteLine("Hello.");
+            
+        }
+    }
+}
